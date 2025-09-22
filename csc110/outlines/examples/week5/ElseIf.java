ae86046ffcdccfc81f79887393e82bc18edd542b
Zach Kohlberg <zach.kohlberg@gmail.com>
2025-09-22 15:34:26 -0400

    update guide for Java 25 and finish week 5 outline

---
+++ csc110/outlines/examples/week5/ElseIf.java
@@ -0,0 +1,13 @@
+// ElseIf.java
+
+void main() {
+    int x = Integer.parseInt(IO.readln("Enter an integer: "));
+
+    if (x > 0) {
+        IO.println("Your number is positive.");
+    } else if (x < 0) {
+        IO.println("Your number is negative.");
+    } else {
+        IO.println("Your number is zero.");
+    }
+}
